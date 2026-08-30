dcf3f68777ae7ac890ccc37f02408a4c4827f214
ClaveroJuanSL <clavero.juan.2025@gmail.com>
2026-08-30 13:57:35 -0300

    Arreglar ReservaController FINAL

--- Controllers/ReservaController.cs
+++ Controllers/ReservaController.cs
@@ -5,27 +5,15 @@ namespace inmobiliaria_lab2_pascual_leyes_delahoz_clavero.Controllers
 {
     public class ReservaController : Controller
     {
-        private readonly IRepositorioReserva repositorioReserva;
-        private readonly IRepositorioInquilino repositorioInquilino;
-
-        private readonly RepositorioInmueble repositorioInmueble;
-        private readonly IConfiguration configuration;
-        private readonly ILogger<ReservaController> logger;
-<<<<<<< HEAD
-        public ReservaController(IRepositorioReserva repositorio, IConfiguration configuration, ILogger<ReservaController> logger)
-        {
-            this.repositorioReserva = repositorioReserva;
-            this.repositorioInquilino = repositorioInquilino;
-            this.repositorioInmueble = repositorioInmueble;
-            this.configuration = configuration;
-=======
+        private readonly IRepositorioReserva repositorio;
         private readonly IRepositorioInquilino repoInquilino;
         private readonly RepositorioInmueble repoInmueble;
+        private readonly IConfiguration configuration;
+        private readonly ILogger<ReservaController> logger;
 
         public ReservaController(IRepositorioReserva repositorio, IRepositorioInquilino repoInquilino, RepositorioInmueble repoInmueble, IConfiguration configuration, ILogger<ReservaController> logger)
         {
             this.repositorio = repositorio;
->>>>>>> Clavero
             this.repoInquilino = repoInquilino;
             this.repoInmueble = repoInmueble;
             this.configuration = configuration;
@@ -35,11 +23,13 @@ namespace inmobiliaria_lab2_pascual_leyes_delahoz_clavero.Controllers
 
         public IActionResult Index()
         {
-            var lista = repositorioReserva.ObtenerLista();
+            var lista = repositorio.ObtenerLista();
             return View(lista);
         }
         public IActionResult Alta()
         {
+            ViewBag.Inquilinos = repoInquilino.ObtenerLista();
+            ViewBag.Inmuebles = repoInmueble.ObtenerLista();
             return View();
         }
         [HttpPost]
@@ -47,17 +37,14 @@ namespace inmobiliaria_lab2_pascual_leyes_delahoz_clavero.Controllers
         {
             if (ModelState.IsValid)
             {
-<<<<<<< HEAD
-                repositorioReserva.Alta(reserva);
-                return RedirectToAction(nameof(Index));
-=======
                 repositorio.Alta(reserva);
->>>>>>> Clavero
+                return RedirectToAction(nameof(Index));  
             }
             ViewBag.Inquilinos = repoInquilino.ObtenerLista();
             ViewBag.Inmuebles = repoInmueble.ObtenerLista();
             return View(reserva);
         }
+
 
     }
 }
